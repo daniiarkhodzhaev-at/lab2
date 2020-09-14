60add13a980df44fd9f9d90fc2867583c7456493
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2020-09-14 09:04:24 +0300

    Done 2

---
+++ 2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import turtle
+
+def main() -> int:
+    turtle.shape("turtle")
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    return 0
+
+if (__name__ == "__main__"):
+    main()
